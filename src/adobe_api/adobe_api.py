0afce9d3758e74270bed996289b1f9de61686ed5
Konstantinos Papadopoulos (Home) <papadopoulospk@gmail.com>
2020-04-11 13:06:45 +0100

    Init

---
+++ src/adobe_api/adobe_api.py
@@ -0,0 +1,165 @@
+from datetime import datetime
+from datetime import timedelta
+import json
+import jwt
+import os
+import requests
+import requests_mock
+
+class aa_client:
+
+    def __init__(self, adobe_org_id, subject_account, client_id, client_secret, account_id, private_key_location = '.ssh/private.key'):
+        self.adobe_auth_host = 'https://ims-na1.adobelogin.com'
+        self.adobe_auth_url = os.path.join(self.adobe_auth_host, 'ims/exchange/jwt')
+        self.adobe_org_id = adobe_org_id
+        self.subject_account = subject_account
+
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.private_key_location = os.path.join(os.path.expanduser('~'), private_key_location)
+        self.account_id = account_id
+
+        self.experience_cloud_metascope = 'https://ims-na1.adobelogin.com/s/ent_analytics_bulk_ingest_sdk'
+
+        self.analytics_url = "https://analytics.adobe.io/api/{}/reports".format(self.account_id)
+        '''
+        self.analytics_header = {           
+                    "X-Api-Key": self.client_id,
+                    "x-proxy-global-company-id": self.account_id,
+                    "Authorization" : "Bearer " + resultjson["access_token"],
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+        } ''' 
+        self.report_object = self._generate_empty_report_object()
+
+    def _read_private_key(self):
+        # Request Access Key 
+        #This Needs to point at where your private key is on the file system
+        keyfile = open(self.private_key_location,'r') 
+        private_key = keyfile.read()
+        return private_key
+
+    def _get_jwtPayload(self, expiration = datetime.utcnow()):
+        jwtPayloadJson = {}
+        jwtPayloadJson['iss'] = self.adobe_org_id
+        jwtPayloadJson['sub'] = self.subject_account
+        jwtPayloadJson[self.experience_cloud_metascope] = True
+        jwtPayloadJson['aud'] = os.path.join(self.adobe_auth_host,'c',self.client_id)
+        jwtPayloadJson["exp"] = expiration + timedelta(minutes=30)
+    
+        return jwtPayloadJson
+
+    def _renew_access_token(self):
+
+        private_key = self._read_private_key()
+        
+        jwtPayloadJson = self._get_jwtPayload()
+        # Encode the jwt Token
+        jwttoken = jwt.encode(jwtPayloadJson, private_key, algorithm='RS256')
+        
+        accessTokenRequestPayload = {
+            'client_id': self.client_id
+            ,'client_secret': self.client_secret
+            , 'jwt_token': jwttoken
+        }
+        result = requests.post(self.adobe_auth_url, data = accessTokenRequestPayload)
+        resultjson = json.loads(result.text)
+        
+        return resultjson['access_token']
+
+    def _get_request_headers(self):
+
+        self.access_token = self._renew_access_token()
+
+        analytics_header = {           
+            "X-Api-Key": self.client_id,
+            "x-proxy-global-company-id": self.account_id,
+            "Authorization" : "Bearer " + self.access_token,
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
+        return analytics_header
+
+    @staticmethod
+    def _generate_empty_report_object():
+        report = {
+            "rsid": "vodafonegroupukprod",
+            "globalFilters": [
+                {
+                    "type": "dateRange",
+                    "dateRange": ""
+                }
+            ],
+            "metricContainer": {
+                "metrics": []
+            },
+            "dimension": "",
+            # "settings": {
+            #     "countRepeatInstances": 'true',
+            #     "limit": 400,
+            #     "page": 1,
+            #     "dimensionSort": "asc",
+            #     "nonesBehavior": "return-nones"
+            # },
+            # "statistics": {
+            #     "functions": [
+            #         "col-max",
+            #         "col-min"
+            #     ]
+            # }
+        }
+        return report
+    
+    @staticmethod
+    def _generate_metric_structure():
+        metric = {
+            "columnId":"",
+            "id": ""
+        }
+        return metric
+
+    @staticmethod
+    def _format_date_range(date_start, date_end):
+        '''Format date range into an API-compatible.
+        2017-12-31T00:00:00.000/2018-01-06T23:59:59.999
+        '''
+        date_start = datetime.strptime(date_start, '%Y-%m-%d')
+        date_start = date_start + timedelta(microseconds=1)
+        date_start = date_start.isoformat('T')
+
+        date_end = datetime.strptime(date_end, '%Y-%m-%d')
+        date_end = date_end + timedelta(hours=23,minutes=59,seconds=59, milliseconds=999)
+        date_end = date_end.isoformat('T')
+        final_date = '{}/{}'.format(date_start[:-7], date_end[:-7])
+        return final_date
+
+    def set_report_suite(self, report_suite_id):
+        self.report_object['rsid'] = report_suite_id
+
+    def get_report(self):
+        analytics_header = self._get_request_headers()
+        pages = requests.post( 
+            self.analytics_url, 
+            headers = analytics_header, 
+            data = json.dumps(self.report_object), 
+        )
+        return pages
+
+
+
+    def add_metric(self, metric_name):
+        metric = self._generate_metric_structure()
+        existing_number_of_metrics = len(self.report_object['metricContainer']['metrics'])
+
+        metric['columnId'] = '{}'.format(existing_number_of_metrics)
+        metric['id'] = metric_name
+
+        self.report_object['metricContainer']['metrics'].append(metric)
+
+    def set_dimension(self, dimension_name):
+        self.report_object['dimension'] = dimension_name
+
+    def set_date_range(self, date_start, date_end):
+        formated_date_range = self._format_date_range(date_start = date_start, date_end = date_end)
+        self.report_object['globalFilters'][0]['dateRange'] = formated_date_range
---
+++ tests/test_sample.py
@@ -0,0 +1,179 @@
+from datetime import datetime
+from datetime import timedelta
+import requests
+import requests_mock
+import json
+import jwt
+import os
+from src.adobe_api.adobe_api import aa_client
+
+test_adobe_org_id= 'fake_org_id'
+test_subject_account = 'fake_subject_account'
+test_client_id = 'fake_client_id' 
+test_client_secret = 'fake_client_secret'
+test_account_id= 'fake_account_id'
+test_access_token = 'fake_access_token'
+test_report_suite_id = 'fake_rsid'
+
+def _generate_adobe_client():
+    client = aa_client(
+        adobe_org_id= test_adobe_org_id, 
+        subject_account = test_subject_account, 
+        client_id = test_client_id ,
+        client_secret = test_client_secret,
+        account_id= test_account_id)
+    return client
+
+def test_client_constructor():
+
+    client = _generate_adobe_client()
+    
+    assert client.adobe_auth_host == 'https://ims-na1.adobelogin.com'
+    assert client.adobe_auth_url == os.path.join(client.adobe_auth_host, 'ims/exchange/jwt')
+    assert client.adobe_org_id == test_adobe_org_id
+    assert client.subject_account == test_subject_account
+
+    assert client.client_id == test_client_id
+    assert client.client_secret == test_client_secret
+    assert client.private_key_location == '/Users/konos/.ssh/private.key'
+    assert client.account_id == test_account_id
+
+    assert client.experience_cloud_metascope == 'https://ims-na1.adobelogin.com/s/ent_analytics_bulk_ingest_sdk'
+
+    assert client.analytics_url == "https://analytics.adobe.io/api/{}/reports".format(test_account_id)
+
+def test_empty_report_object():
+    expected_report_object = {
+            "rsid": "vodafonegroupukprod",
+            "globalFilters": [
+                {
+                    "type": "dateRange",
+                    "dateRange": ""
+                }
+            ],
+            "metricContainer": {
+                "metrics": []
+            },
+            "dimension": ""
+        }
+    actual_report_object = aa_client._generate_empty_report_object()
+    assert actual_report_object == expected_report_object
+
+def test_jwtPayload():
+    client = _generate_adobe_client()
+
+    jwt_expiration = datetime(2020, 4, 8, 20, 30, 30, 107868)
+
+    expected_jwt = {
+        'iss': test_adobe_org_id, 
+        'sub': test_subject_account, 
+        'https://ims-na1.adobelogin.com/s/ent_analytics_bulk_ingest_sdk': True, 
+        'aud': 'https://ims-na1.adobelogin.com/c/{}'.format(test_client_id), 
+        'exp': jwt_expiration + timedelta(minutes = 30)
+        }
+
+    assert client._get_jwtPayload(jwt_expiration) == expected_jwt
+
+def test_renew_access_token(mocker):
+    ACCESS_TOKEN_VALUE = 'test token value'
+    # adapter = requests_mock.Adapter()
+    # mock reading private key
+    client = _generate_adobe_client()
+
+    client._read_private_key = mocker.Mock(return_value = 'test_key')
+    jwt.encode = mocker.Mock(return_value = 'jwt_encoded')
+    assert client._read_private_key() == 'test_key'
+
+    test_response_text = json.dumps({"token_type":"bearer","access_token":ACCESS_TOKEN_VALUE,"expires_in":86399995})
+    
+    # Generate fake response
+    adapter = requests_mock.Adapter()
+    adapter.register_uri('GET', 'mock://test.com/path', text=test_response_text)
+    session = requests.Session()
+    session.mount('mock', adapter)
+    test_response = session.get('mock://test.com/path')
+    
+    # Patch request and fake response
+    mocker.patch("requests.post", return_value = test_response)
+    
+    assert ACCESS_TOKEN_VALUE == client._renew_access_token()
+
+def test_failed_access_token(mocker):
+    error_message = {'error_description': 'JWT token is incorrectly formatted, and can not be decoded.',
+                    'error': 'invalid_token'}
+    # TODO: write test case
+    pass
+
+
+def test_get_request_headers(mocker):
+    
+    client = _generate_adobe_client()
+    client._renew_access_token = mocker.Mock(return_value = test_access_token)
+
+    expected_analytics_header = {           
+            "X-Api-Key": test_client_id,
+            "x-proxy-global-company-id": test_account_id,
+            "Authorization" : "Bearer " + test_access_token,
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
+    assert client._get_request_headers() == expected_analytics_header
+
+def test_format_date_range():
+    client = _generate_adobe_client()
+    
+    #Case 1: different dates
+    start_date ='2017-01-31'
+    end_date = '2020-12-31'
+    expected_date_format = '2017-01-31T00:00:00/2020-12-31T23:59:59'
+
+    assert expected_date_format == client._format_date_range(date_start = start_date, date_end = end_date)
+    
+    client.set_date_range(start_date, end_date)
+    assert client.report_object['globalFilters'][0]['dateRange'] == expected_date_format
+
+    #Case 2: Same dates
+    start_date = '2017-01-31'
+    end_date = '2020-01-31'
+    expected_date_format = '2017-01-31T00:00:00/2020-01-31T23:59:59'
+
+    assert expected_date_format == client._format_date_range(date_start = start_date, date_end = end_date)
+
+    client.set_date_range(start_date, end_date)
+    assert client.report_object['globalFilters'][0]['dateRange'] == expected_date_format
+
+def test_set_report_suite():
+    client = _generate_adobe_client()
+
+    client.set_report_suite(test_report_suite_id)
+
+    assert client.report_object['rsid'] == test_report_suite_id
+
+def test_metric_add():
+    client = _generate_adobe_client()
+
+    test_metric_name = "metrics/pageviews"
+    expected_metric = {
+            "columnId":"0",
+            "id": test_metric_name
+        }
+    client.add_metric(test_metric_name)
+
+    assert expected_metric == client.report_object['metricContainer']['metrics'][0]
+
+    test_metric_name = "metrics/visits"
+    expected_metric = {
+            "columnId":"1",
+            "id": test_metric_name
+        }
+    client.add_metric(test_metric_name)
+    assert expected_metric == client.report_object['metricContainer']['metrics'][1]
+
+def test_dimension_set():
+    client = _generate_adobe_client()
+    test_dimension_name = 'variables/daterangeday'
+
+    client.set_dimension(test_dimension_name)
+
+    assert test_dimension_name == client.report_object['dimension']
