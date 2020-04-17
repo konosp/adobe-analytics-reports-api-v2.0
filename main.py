c646234ee5d58271ba9c0f4684599f17e4a91efa
Konstantinos Papadopoulos (Home) <papadopoulospk@gmail.com>
2020-04-17 22:50:49 +0100

    Main fixes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from src.adobe_api.adobe_api import aa_client
+from src.analytics.mayhem.adobe import analytics_client
 
 
-aa = aa_client(adobe_org_id = adobe_org_id, subject_account = subject_account, 
+aa = analytics_client(adobe_org_id = adobe_org_id, subject_account = subject_account, 
     client_id = client_id, client_secret = client_secret, account_id = account_id, private_key_location = private_key_location)
 
-aa.set_report_suite(report_suite_id = 'vodafonegroupukprod')
+aa.set_report_suite(report_suite_id = report_suite_id)
 aa.add_metric(metric_name= 'metrics/visits')
 aa.add_metric(metric_name= 'metrics/orders')
 aa.add_metric(metric_name= 'metrics/event3')
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="analytics_mayhem_adobe",
+    version="0.0.1",
+    author="Konstantinos Papadopoulos",
+    author_email="info@analyticsmayhem.com",
+    description="Manage Adobe Analytics Reports API v2 requests to build reports programmatically.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/konosp/adobe-analytics-reports-api-v2.0",
+    package_dir={"": "src"},
+    packages=find_namespace_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pandas",
+        "requests",
+        "PyJWT"
+    ],
+    python_requires='>=3.6',
+)
